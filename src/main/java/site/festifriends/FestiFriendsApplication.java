cabd6dba211d7e4dd1e0ecde089ac346d6166938
JWbase <jungwoo1002@gmail.com>
2025-05-20 10:42:34 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/site/festifriends/FestiFriendsApplication.java
@@ -0,0 +1,13 @@
+package site.festifriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestiFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/festifriends/FestiFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.festifriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestiFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
