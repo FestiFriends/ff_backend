5dfce777bd49a5808e3ff54842d25d8d6cf4c865
dnjsals45 <dnjsals45@naver.com>
2025-05-26 09:48:49 +0900

    ✏️chore: FestivalBookmark 오타 수정(member_id -> festival_id)

--- src/main/java/site/festifriends/entity/FestivalBookmark.java
+++ src/main/java/site/festifriends/entity/FestivalBookmark.java
@@ -20,6 +20,6 @@ public class FestivalBookmark extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "festival_id", nullable = false)
     private Festival festival;
 }
