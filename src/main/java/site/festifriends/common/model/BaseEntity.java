ea1782d10698a0cd4179f5cc554d1d4f70aa31bf
dnjsals45 <dnjsals45@naver.com>
2025-05-26 09:35:17 +0900

    ✨feat: 엔티티 Auditing 기본 모델 작성

---
+++ src/main/java/site/festifriends/common/model/BaseEntity.java
@@ -0,0 +1,33 @@
+package site.festifriends.common.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Comment("생성 시간")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    @Comment("수정 시간")
+    private LocalDateTime updatedAt;
+}
