793a0d9b25da7acca3c9dc3ece90cdae97b25f77
dnjsals45 <dnjsals45@naver.com>
2025-05-27 14:02:23 +0900

    ✨feat: Audit,RestClient 설정 파일 추가

---
+++ src/main/java/site/festifriends/common/config/AuditConfig.java
@@ -0,0 +1,10 @@
+package site.festifriends.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditConfig {
+
+}
