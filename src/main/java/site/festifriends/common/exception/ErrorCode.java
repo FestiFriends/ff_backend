4f61de6867391d553de36d9dca4679a87da78815
JWbase <jungwoo1002@gmail.com>
2025-05-29 00:42:22 +0900

    ✨feat: 비즈니스 예외 처리 및 응답 래퍼에 오류 처리 메서드 추가

--- src/main/java/site/festifriends/common/exception/ErrorCode.java
+++ src/main/java/site/festifriends/common/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-    ;
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
