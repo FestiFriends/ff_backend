df1ac7e7a76881882b5d64fcd994cc7b0608ccba
JWbase <jungwoo1002@gmail.com>
2025-05-29 15:42:50 +0900

    ✨refactor: 불필요한 클래스 제거

--- src/main/java/site/festifriends/domain/group/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package site.festifriends.domain.group.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import site.festifriends.entity.Group;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
