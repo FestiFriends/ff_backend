445ba0f5027093ebcd5061a7cda079f7cf9ad3db
JWbase <jungwoo1002@gmail.com>
2025-05-30 14:58:16 +0900

    ✨ feat: Add isNewUser property to AuthInfo and AuthResponse classes

--- src/main/java/site/festifriends/domain/auth/AuthInfo.java
+++ src/main/java/site/festifriends/domain/auth/AuthInfo.java
@@ -1,5 +1,6 @@
 package site.festifriends.domain.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,8 @@ public class AuthInfo {
 
     private final String accessToken;
     private final String refreshToken;
+
+    @JsonProperty("isNewUser")
     private final Boolean isNewUser;
-    
+
 }
