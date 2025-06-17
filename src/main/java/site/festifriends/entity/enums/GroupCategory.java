9debd734df177fdc19b0ce89c510ae42849af1a1
JWbase <jungwoo1002@gmail.com>
2025-06-17 20:21:24 +0900

    ♻️ refactor: Update ROOM_SHARE description for consistency

--- src/main/java/site/festifriends/entity/enums/GroupCategory.java
+++ src/main/java/site/festifriends/entity/enums/GroupCategory.java
@@ -6,7 +6,7 @@ package site.festifriends.entity.enums;
 public enum GroupCategory {
     COMPANION("같이 동행"),
     RIDE_SHARE("같이 탑승"),
-    ROOM_SHARE("같이 숙소");
+    ROOM_SHARE("같이 숙박");
 
     private final String description;
 
